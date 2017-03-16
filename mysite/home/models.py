26789a9cbd8e425ea794a7b6e30f8435c9cf1004
Leonid Shvechikov <shvechikov@gmail.com>
2017-03-16 12:07:50 +0300

    implement MobileMixin

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -3,7 +3,22 @@ from __future__ import absolute_import, unicode_literals
 from django.db import models
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.utils import camelcase_to_underscore
 
 
-class HomePage(Page):
+class MobileMixin:
+    def get_template(self, request, *args, **kwargs):
+        if request.is_ajax() and self.ajax_template:
+            return self.ajax_template
+
+        cls = self.__class__
+        template = "%s/%s/%s.html" % (
+            cls._meta.app_label,
+            request.flavour,
+            camelcase_to_underscore(cls.__name__),
+        )
+        return template
+
+
+class HomePage(MobileMixin, Page):
     pass
